39a7bcb4aeb464f26fa2f9921867166f2bf04d43
Itay7414 <itaykoko7414@gmail.com>
2023-06-12 17:31:08 +0300

    version 1.5 - node.js, express, javascipt

--- index.js
+++ index.js
@@ -1,54 +1,62 @@
-// JavaScript code for fetching top hot sales data and populating the page
+// Client-side code
 document.addEventListener('DOMContentLoaded', function() {
-    // Fetch top hot sales data from the server using AJAX
-    fetch('/api/top-sales')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        // Populate the top sales section with the received data
-        var topSalesContainer = document.getElementById('top-sales');
-        data.forEach(function(product) {
-          var card = createProductCard(product);
-          topSalesContainer.appendChild(card);
-        });
-      })
-      .catch(function(error) {
-        console.log('Error fetching top sales:', error);
+  // Fetch top hot sales data from the server using AJAX
+  fetch('/api/top-sales')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      // Populate the top sales section with the received data
+      var topSalesContainer = document.getElementById('top-sales');
+      data.forEach(function(product) {
+        var card = createProductCard(product);
+        topSalesContainer.appendChild(card);
       });
-  });
-  
-  // Helper function to create a product card element
-  function createProductCard(product) {
-    var card = document.createElement('div');
-    card.className = 'product-card';
-    
-    var img = document.createElement('img');
-    img.src = product.image;
-    card.appendChild(img);
-    
-    var title = document.createElement('h3');
-    title.textContent = product.name;
-    card.appendChild(title);
-    
-    var price = document.createElement('p');
-    price.textContent = 'Price: $' + product.price;
-    card.appendChild(price);
-    
-    var addToCartBtn = document.createElement('button');
-    addToCartBtn.textContent = 'Add to Cart';
-    addToCartBtn.addEventListener('click', function() {
-      // Add the product to the cart
-      addToCart(product);
+    })
+    .catch(function(error) {
+      console.log('Error fetching top sales:', error);
     });
-    card.appendChild(addToCartBtn);
-    
-    return card;
-  }
-  
-  // Function to add a product to the cart (implement according to your needs)
-  function addToCart(product) {
-    // Implement the functionality to add a product to the cart
-    // This can involve making an AJAX request to the server to update the cart
-  }
-  
+});
+
+// Helper function to create a product card element
+function createProductCard(product) {
+  var card = document.createElement('div');
+  card.className = 'card';
+
+  var img = document.createElement('img');
+  img.src = product.image;
+  img.className = 'card-img-top';
+  card.appendChild(img);
+
+  var cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+
+  var title = document.createElement('h5');
+  title.className = 'card-title';
+  title.textContent = product.name;
+  cardBody.appendChild(title);
+
+  var price = document.createElement('p');
+  price.className = 'card-text';
+  price.textContent = 'Price: $' + product.price;
+  cardBody.appendChild(price);
+
+  var addToCartBtn = document.createElement('button');
+  addToCartBtn.className = 'btn btn-primary';
+  addToCartBtn.textContent = 'Add to Cart';
+  addToCartBtn.addEventListener('click', function() {
+    // Add the product to the cart
+    addToCart(product);
+  });
+  cardBody.appendChild(addToCartBtn);
+
+  card.appendChild(cardBody);
+
+  return card;
+}
+
+// Function to add a product to the cart (implement according to your needs)
+function addToCart(product) {
+  // Implement the functionality to add a product to the cart
+  // This can involve making an AJAX request to the server to update the cart
+}
